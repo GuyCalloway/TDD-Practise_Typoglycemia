1f81a65bcdf4ff38f538df4e6d671193977e6534
GuyCalloway <guyfcalloway@gmail.com>
2019-10-24 22:27:24 +0100

    10th test passing, refactored, renamed

--- lib/typog.rb
+++ lib/typog.rb
@@ -1,20 +1,30 @@
 def typo_g(string)
-   z = ""
-   x = false
-
+ 
   if string.length <= 3
     return string
   else
     words = string.split(" ")
-    
     sorted_words = words.map { |word| 
-        y = word.split("")
-    x = [y.shift]
-    y.pop
-    z = y.sort { |a, b| a.match(/\.|,|'|-/) ? 0 : b.match(/\.|,|'|-/) ? 0 : a <=> b} 
-    x << z
-    x << word[-1]
-    x.join("")
+        letters = word.split("")
+        exclusions = []
+        letters.each_with_index do |a, i|
+          if a.match(/\.|,|'|-/) 
+            letters.delete_at(i)
+            exclusions << [a, i]
+          end
+        end
+
+    letter_array = [letters.shift]
+    letters.pop
+    sorted_letters = letters.sort { |a, b|  a <=> b} 
+    letter_array << sorted_letters
+    letter_array << word[-1]
+    letter_array.flatten!
+    
+    exclusions.each { |a| if a 
+                letter_array.insert(a[1], a[0])
+    end }
+    letter_array.join("")
     }
   end
   return sorted_words.join(" ")
--- spec/typog_spec.rb
+++ spec/typog_spec.rb
@@ -13,23 +13,31 @@ describe "typo g" do
         expect(typo_g("hey")).to eq("hey")
     end
 
-    it "returns 3 letter string" do
+    it "returns scrambled string" do
         expect(typo_g("heay")).to eq("haey")
     end
 
-    it "returns 3 letter string" do
+    it "returns scrambled string" do
         expect(typo_g("heacy")).to eq("hacey")
     end
 
-    it "returns 3 letter string" do
+    it "returns scrambled string" do
         expect(typo_g("heacy heacy")).to eq("hacey hacey")
     end
 
-    it "returns 3 letter string" do
+    it "returns scrambled string" do
         expect(typo_g("heacy heacy smrat asre")).to eq("hacey hacey samrt arse")
     end
 
-    it "returns 3 letter string" do
+    it "returns scrambled string" do
         expect(typo_g("shan't")).to eq("sahn't")
+    end
+
+    it "returns scrambled string" do
+        expect(typo_g("-dcba")).to eq("-dbca")
+    end
+
+    it "returns scrambled string" do
+        expect(typo_g("tik-tak")).to eq("tai-ktk")
     end
 end
